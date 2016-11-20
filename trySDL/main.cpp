269d901d5891fdebdfeef2ecbffce680d07318d3
Ziwen Chen <arthurhero@Ziwens-MacBook.local>
2016-11-19 22:58:23 -0600

    Initial Commit

---
+++ trySDL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  trySDL
+//
+//  Created by Ziwen Chen on 11/19/16.
+//  Copyright © 2016 Ziwen Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
